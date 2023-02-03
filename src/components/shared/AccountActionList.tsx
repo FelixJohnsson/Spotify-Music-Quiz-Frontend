3dd6b9d70ebbedc5030cbd6518384982a09dffda
Felix Johnsson <felix.johnsson@proton.me>
2023-02-03 20:13:44 +0100

    Removed unnecessary action in sidebar

--- src/components/shared/AccountActionList.tsx
+++ src/components/shared/AccountActionList.tsx
@@ -1,22 +1,9 @@
 import * as React from 'react';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import Collapse from '@mui/material/Collapse';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import Settings from '@mui/icons-material/Settings';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
 
 const AccountActionList = () => {
-  const [open, setOpen] = React.useState(true);
-
-  const handleClick = () => {
-    setOpen(!open);
-  };
-
   return (
     <List
       sx={{ width: '100%', maxWidth: 360, color: 'white'}}
@@ -26,23 +13,10 @@ const AccountActionList = () => {
       <ListItemButton>
         <ListItemText primary="Settings" />
       </ListItemButton>
-      <ListItemButton>
 
-      </ListItemButton>
-      <ListItemButton onClick={handleClick}>
+      <ListItemButton>
         <ListItemText primary="Account" />
-        {open ? <ExpandLess /> : <ExpandMore />}
       </ListItemButton>
-      <Collapse in={!open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          <ListItemButton sx={{ pl: 4 }}>
-             <ListItemText primary="Remove account" />
-          </ListItemButton>
-          <ListItemButton sx={{ pl: 4 }}>
-             <ListItemText primary="Edit account" />
-          </ListItemButton>
-        </List>
-      </Collapse>
     </List>
   );
 }
